8c42f4293a99168318d5ff1900b44dd1dcabeadb
Lars Knoke <l.knoke@colorplus.de>
2022-07-25 14:28:59 +0200

    Autorenkorrekturen

--- pages/leistungen.js
+++ pages/leistungen.js
@@ -104,8 +104,9 @@ function Leistungen() {
             </span>
             <span className="border-t border-zad-blue-200 pt-4 px-3">
               Mit der Gründung des Unternehmens ZAD TECH wurde eine neue
-              Software "PflegeSuite" für den ambulanten Pflegedienst entwickelt.
-              Die intelligente Verbindung von Tourenplanung mit Factoring.
+              Software &quot;PflegeSuite&quot; für den ambulanten Pflegedienst
+              entwickelt. Die intelligente Verbindung von Tourenplanung mit
+              Factoring.
             </span>
             <img
               className="pt-8 w-3/4 self-center"
