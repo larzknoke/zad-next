11a0473a55b97187b1729ac5446993f53d828c32
Lars Knoke <l.knoke@colorplus.de>
2023-01-27 13:05:30 +0100

    Zeilenumbruch

--- pages/stellenangebote.js
+++ pages/stellenangebote.js
@@ -76,7 +76,7 @@ function Stellenangebote() {
             Sie haben kein passendes Stellenangebot gefunden oder sind
             Quereinsteiger? Dann überzeugen Sie uns mit Ihrer Initativbewerbung
             und gehen mit uns neue Wege im Büro oder der Informationstechnik.
-            Bitte beachten Sie, dass wir für Bewerbungen ausschließlich
+            <br /> Bitte beachten Sie, dass wir für Bewerbungen ausschließlich
             Datei-Formate im PDF akzeptieren. Andere Datei-Formate sind nicht
             zulässig.
           </p>
