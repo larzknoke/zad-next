e11aa245e89385d10d955b7ba937f23146cd36eb
Lars Knoke <l.knoke@colorplus.de>
2023-05-08 13:23:33 +0200

    placeholder kleiner

--- components/sideContact.js
+++ components/sideContact.js
@@ -64,13 +64,17 @@ export default function SideContact() {
           <h5 className="text-zad-blue-600 mb-4">
             Gern rufen wir Sie auch zurück:
           </h5>
-          <form method="post" onSubmit={handleOnSubmit}>
+          <form
+            method="post"
+            onSubmit={handleOnSubmit}
+            className="side-contact-form"
+          >
             <div className="form-group mb-6">
               <input
                 type="text"
                 className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded-sm transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                 id="inputTelefon"
-                placeholder="Unter welcher Telefonnummer dürfen wir Sie kontaktieren?"
+                placeholder="Ihre Telefonnummer für unseren Rückruf"
                 name="telefon"
               />
             </div>
@@ -79,7 +83,7 @@ export default function SideContact() {
                 type="text"
                 className="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded-sm transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
                 id="inputUhrzeit"
-                placeholder="Wann können wir Sie gut erreichen (Tag / Uhrzeit) ?"
+                placeholder="Wann können wir Sie erreichen (Tag / Uhrzeit)"
                 name="uhrzeit"
               />
             </div>
@@ -136,7 +140,7 @@ export default function SideContact() {
                 required={true}
               />
               <label
-                className="form-check-label inline-block text-zad-blue-600 text-sm"
+                className="form-check-label inline-block text-zad-blue-600 text-sm cursor-pointer"
                 htmlFor="inputDatenschutz"
               >
                 Ja, ich habe die{" "}
