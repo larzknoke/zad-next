b616538664aa345f1f4e0a6e563015553e996c0b
Lars Knoke <l.knoke@colorplus.de>
2024-05-30 13:58:46 +0200

    Haftungssauschluss raus

--- pages/impressum.js
+++ pages/impressum.js
@@ -79,44 +79,6 @@ function Impressum() {
         <br />
         37154 Northeim
       </p>
-      <h3 className="mt-8">Haftungsausschluss</h3>
-      <p>
-        Haftung für Inhalten
-        <br />
-        Die Inhalte dieser Webseite wurden sorgfältig und nach aktuellem
-        Kenntnisstand erstellt. Als Diensteanbieter sind wir nur im Rahmen der
-        allgemeinen Gesetze verantwortlich, insbesondere für eigene Inhalte nach
-        § 16 TMG. Für fremde Inhalte sind wir nach den §§ 8 bis 10 TMG
-        frühestens ab Kenntniserlangung einer konkreten Rechtverletzung
-        verantwortlich. Wir behalten uns vor, die informatorischen Inhalte
-        vollständig oder teilweise zu ändern oder löschen, soweit vertragliche
-        Verpflichtungen unberührt bleiben.
-      </p>
-      <p>
-        Verweisung auf fremde Seiten
-        <br />
-        Inhalte fremder Webseiten, auf die wir direkt oder indirekt verweisen,
-        liegen außerhalb unseres Verantwortungsbereiches. Zum Zeitpunkt der
-        Linksetzung waren für uns keine rechtswidrigen Inhalte auf den
-        verlinkten Webseiten erkennbar. Da wir auf die Gestaltung der Inhalte
-        der verlinkten Webseiten keinerlei Einfluss haben, distanzieren wir uns
-        ausdrücklich von allen Inhalten, die nach der Linksetzung verändert
-        wurden. Für alle Inhalte und insbesondere Schäden, die aus der Nutzung
-        der in den verlinkten Webseiten aufrufbaren Informationen entstehen,
-        haftet alleine der Anbieter der Seite, auf welche verlinkt wurde.
-        Erhalten wir von illegalen, rechtswidrigen oder fehlerhaften Inhalten
-        auf Webseiten Kenntnis, die wir verlinken, werden wir die Verlinkung
-        aufheben.
-      </p>
-      <p>
-        Urheberrechte
-        <br />© Copyright ZAD-GmbH, Northeim (Deutschland). Alle Rechte
-        vorbehalten. Texte, Bilder, Grafiken und Animationen sowie deren
-        Anordnung auf der ZAD-GmbH Website unterliegen dem Schutz und den Normen
-        des deutschen Urheberrechts. Der Inhalt dieser Website darf nicht zu
-        kommerziellen Zwecken kopiert, verbreitet, verändert oder Dritten
-        zugänglich gemacht werden.
-      </p>
     </div>
   );
 }
